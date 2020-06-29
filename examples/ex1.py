9dd74be6f8ba95340f1b3d711bb0221c62d05a97
Juan Kevin Trujillo <me@juankevintrujillo.com>
2020-06-29 02:16:24 +0100

    Example 1 finished!

---
+++ examples/ex1.py
@@ -0,0 +1,15 @@
+# Comment in one line
+
+"""
+Comment
+in many lines
+"""
+
+print("----------Example1----------")
+name = "Juan Kevin Trujillo"
+age = 27
+adult = True
+
+print("Name? = ", name)
+print("Age? = ", age)
+print("Adult? = ", adult)
