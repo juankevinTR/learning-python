250247ef6b582821194202cc82d80c9e9d007200
Juan Kevin Trujillo <me@juankevintrujillo.com>
2020-06-29 23:18:56 +0100

    Exercise 1 finished!

---
+++ exercises/exer1.py
@@ -0,0 +1,16 @@
+# Highest number of three
+"""
+Calculate the highest number, the lowest number and the intermediate of three numbers that we will ask to the user
+"""
+
+numberOne = int(input("Introduce the first number: "))
+numberTwo = int(input("Introduce the second number: "))
+numberThree = int(input("Introduce the third number: "))
+
+sortedNumbers = [numberOne, numberTwo, numberThree]
+sortedNumbers.sort()
+
+print("\n----------Results----------")
+print(f"Lowest number: {sortedNumbers[0]}")
+print(f"iIntermediate number: {sortedNumbers[1]}")
+print(f"Highest number : {sortedNumbers[2]}")
